a43af1e422f2d8fc7e0f842fb9af906b816be4d5
Stepan Redka <stepan@railsmuffin.com>
2020-01-27 17:16:41 +0300

    chore: remove redundant parentheses

--- lib/uploadcare/service/upload/signature_generator.rb
+++ lib/uploadcare/service/upload/signature_generator.rb
@@ -1,14 +1,13 @@
-# This file generates signatures for protected uploads
-# https://uploadcare.com/docs/api_reference/upload/signed_uploads/
-
 require 'digest'
 
 module Uploadcare
+  # This class generates signatures for protected uploads
+  # https://uploadcare.com/docs/api_reference/upload/signed_uploads/
   module Upload
     class SignatureGenerator
       def self.call
-        expires_at = Time.now.to_i + UPLOAD_SIGNATURE_LIFETIME
-        to_sign = SECRET_KEY + (expires_at).to_s
+        expires_at = Time.now.to_i + 60 * 30
+        to_sign = SECRET_KEY + expires_at.to_s
         signature = Digest::MD5.hexdigest(to_sign)
         {
           'signature': signature,
