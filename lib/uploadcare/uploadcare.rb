b43780a8dc90f47492677785a86fa5be3ba34537
Stepan <stepan@railsmuffin.com>
2020-02-27 17:49:27 +0300

    feat: basic API

--- lib/uploadcare/uploadcare.rb
+++ lib/uploadcare/uploadcare.rb
@@ -5,6 +5,7 @@ require 'dry-configurable'
 require 'api_struct_settings'
 require 'uploadcare_configuration'
 require 'default_configuration'
+require 'api/api'
 Gem.find_files('client/**/*.rb').each { |path| require path }
 Gem.find_files('concerns/**/*.rb').each { |path| require path }
 Gem.find_files('entity/**/*.rb').each { |path| require path }
