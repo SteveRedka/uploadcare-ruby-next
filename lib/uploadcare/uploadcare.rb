ede17caf0874dee3a1bf6214cf397aff171938eb
Stepan <stepan@railsmuffin.com>
2020-03-04 16:06:52 +0300

    refactor: move headers and upload params generators into Param folder

--- lib/uploadcare/uploadcare.rb
+++ lib/uploadcare/uploadcare.rb
@@ -9,7 +9,7 @@ require 'api/api'
 Gem.find_files('client/**/*.rb').each { |path| require path }
 Gem.find_files('concerns/**/*.rb').each { |path| require path }
 Gem.find_files('entity/**/*.rb').each { |path| require path }
-Gem.find_files('header/**/*.rb').each { |path| require path }
+Gem.find_files('param/**/*.rb').each { |path| require path }
 Gem.find_files('service/**/*.rb').each { |path| require path }
 
 # Ruby wrapper for Uploadcare API
