1ceeecd6f937bfd376f298ffc52fb26ffb715c46
Stepan <stepan@railsmuffin.com>
2020-03-06 20:04:49 +0300

    feat: File::copy - instance syntax sugar

--- lib/uploadcare/client/file_client.rb
+++ lib/uploadcare/client/file_client.rb
@@ -20,31 +20,16 @@ module Uploadcare
     def info(uuid)
       get(uri: "/files/#{uuid}/")
     end
-    alias :file :info
+    alias file info
 
     # 'copy' method is used to copy original files or their modified versions to default storage.
     # Source files MAY either be stored or just uploaded and MUST NOT be deleted.
     # https://uploadcare.com/api-refs/rest-api/v0.5.0/#operation/copyFile
 
     def copy(**options)
+      options[:source] = options[:source].gsub(/\/-.*/, '') if options[:strip_operations]
       body = options.compact.to_json
       post(uri: '/files/', content: body)
-    end
-
-    # Copies file to current project
-    # source can be UID or full CDN link
-    # https://uploadcare.com/api-refs/rest-api/v0.5.0/#operation/copyFile
-
-    def local_copy(source, store: false)
-      copy(source: source, store: store)
-    end
-
-    # copy file to different project
-    # source can be UID or full CDN link
-    # https://uploadcare.com/api-refs/rest-api/v0.5.0/#operation/copyFile
-
-    def remote_copy(source, target, make_public: false, pattern: '${default}')
-      copy(source: source, target: target, make_public: make_public, pattern: pattern)
     end
 
     alias request_delete delete
